6820f23178643ac57eadfdbda439078825e6f437
oxalica <oxalicc@pm.me>
2023-12-04 18:18:49 +0800

    Asynchronous open

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use std::cell::RefCell;
 use std::collections::BTreeMap;
 use std::ffi::CString;
-use std::fmt;
-use std::os::unix::ffi::OsStringExt;
 use std::path::PathBuf;
+use std::sync::Arc;
+use std::{fmt, process};
 
 use bytesize::ByteSize;
 use clap::Parser;
@@ -11,6 +11,13 @@ use hdrhistogram::Histogram;
 use io_uring::types::Fd;
 use io_uring::{opcode, IoUring};
 use miniserde::Serialize;
+use rayon::Scope;
+use rustix::cstr;
+use rustix::fd::{AsRawFd, FromRawFd, OwnedFd};
+use rustix::fs::{
+    openat, AtFlags, FileType, Mode, OFlags, RawDir, RawDirEntry, Statx, StatxFlags, CWD,
+};
+use rustix::io::Errno;
 
 #[derive(Debug, clap::Parser)]
 #[command(version, about)]
@@ -77,129 +84,184 @@ impl fmt::Display for Output {
     }
 }
 
-const IO_URING_ENTRIES: usize = 32;
+const IO_URING_ENTRIES: usize = 64;
+const BUF_MASK_ALL: u64 = !0;
+const INIT_DIR_BUF_LEN: usize = 4 << 10; // 4KiB
+
+scoped_tls::scoped_thread_local!(static WORKER: RefCell<Worker>);
 
 struct Worker {
     uring: IoUring,
-    bufs: [libc::statx; IO_URING_ENTRIES],
-    paths: [Option<CString>; IO_URING_ENTRIES],
+    bufs: [Statx; IO_URING_ENTRIES],
+    params: [Option<(Arc<OwnedFd>, CString)>; IO_URING_ENTRIES],
     active_mask: u64,
     recorder: hdrhistogram::sync::Recorder<u64>,
     include_empty: bool,
+    dirent_buf: Vec<u8>,
 }
 
 // Pending operations must be completed before dropping the buffer.
 impl Drop for Worker {
     fn drop(&mut self) {
-        let ongoing_cnt = self.active_mask.count_ones() as usize;
-        if ongoing_cnt > 0 {
-            self.uring.submit_and_wait(ongoing_cnt).unwrap();
-            self.handle_completed();
+        if self.active_mask != 0 {
+            eprintln!("tasks still active: {:b}", self.active_mask);
+            std::process::abort();
         }
-        assert_eq!(self.active_mask, 0);
     }
 }
 
 impl Worker {
     fn new(recorder: hdrhistogram::sync::Recorder<u64>, include_empty: bool) -> Self {
-        const NONE: Option<CString> = None; // Workaround of const blocks.
+        const NONE: Option<(Arc<OwnedFd>, CString)> = None; // Workaround of const blocks.
         let uring = IoUring::new(IO_URING_ENTRIES.try_into().unwrap()).unwrap();
         Self {
             uring,
             bufs: [unsafe { std::mem::zeroed() }; IO_URING_ENTRIES],
-            paths: [NONE; IO_URING_ENTRIES],
+            params: [NONE; IO_URING_ENTRIES],
             active_mask: 0,
             recorder,
             include_empty,
+            dirent_buf: Vec::with_capacity(INIT_DIR_BUF_LEN),
         }
     }
 
-    fn submit(&mut self, path: PathBuf) {
-        {
-            let mut sub = self.uring.submission();
+    fn enqueue(&mut self, dirfd: Arc<OwnedFd>, ent: RawDirEntry<'_>, s: &Scope<'_>) {
+        if ent.file_name() == cstr!(".") || ent.file_name() == cstr!("..") {
+            return;
+        }
 
-            let buf_idx = self.active_mask.trailing_ones() as usize;
-            assert!(buf_idx < IO_URING_ENTRIES);
-            self.active_mask |= 1 << buf_idx;
+        // If full, flush and wait.
+        if self.active_mask == BUF_MASK_ALL {
+            self.submit_and_complete(1, s);
+        }
 
-            let mut path = path.into_os_string().into_vec();
-            path.push(b'\0');
-            let path = CString::from_vec_with_nul(path).unwrap();
+        // Always allocate a buffer, even for directories, since we always need to keep
+        // parameters stored and alive.
+        let buf_idx = self.active_mask.trailing_ones() as usize;
+        assert!(buf_idx < IO_URING_ENTRIES);
+        self.active_mask |= 1 << buf_idx;
 
-            let dirfd = Fd(libc::AT_FDCWD);
-            let pathname = path.as_c_str().as_ptr();
+        // Take ownership of parameters.
+        let (dirfd, filename) =
+            self.params[buf_idx].insert((dirfd.clone(), ent.file_name().to_owned()));
+        let dirfd = Fd(dirfd.as_raw_fd());
+        let filename = filename.as_ptr();
+
+        let op = if ent.file_type() != FileType::Directory {
+            // File.
             let buf = &mut self.bufs[buf_idx];
-            buf.stx_mask = libc::STATX_SIZE;
-            self.paths[buf_idx] = Some(path); // Keep the pathname string alive.
-            unsafe {
-                let op = &opcode::Statx::new(dirfd, pathname, (buf as *mut libc::statx).cast())
-                    .flags(libc::AT_SYMLINK_NOFOLLOW)
-                    .build()
-                    .user_data(buf_idx as u64);
-                sub.push(op).unwrap();
-            }
+            buf.stx_mask = StatxFlags::SIZE.bits();
+            opcode::Statx::new(dirfd, filename, (buf as *mut Statx).cast())
+                .flags(AtFlags::SYMLINK_NOFOLLOW.bits() as i32)
+                .build()
+                .user_data(buf_idx as u64)
+        } else {
+            // Directory.
+            opcode::OpenAt::new(dirfd, filename)
+                .flags(
+                    (OFlags::RDONLY | OFlags::DIRECTORY | OFlags::CLOEXEC | OFlags::NOFOLLOW).bits()
+                        as _,
+                )
+                .mode(Mode::empty().bits())
+                .build()
+                .user_data(!(buf_idx as u64))
+        };
+        unsafe {
+            self.uring.submission().push(&op).unwrap();
         }
-
-        let is_full = self.active_mask == (1 << IO_URING_ENTRIES) - 1;
-        self.uring
-            .submit_and_wait(if is_full { 1 } else { 0 })
-            .unwrap();
-        self.handle_completed();
     }
 
-    fn handle_completed(&mut self) {
+    fn submit_and_complete(&mut self, want: usize, s: &Scope<'_>) {
+        self.uring.submit_and_wait(want).unwrap();
         for ent in self.uring.completion() {
-            let buf_idx = ent.user_data() as usize;
+            let data = ent.user_data() as i64;
+            let buf_idx = if data >= 0 { data } else { !data } as usize;
             assert!(buf_idx < IO_URING_ENTRIES);
             self.active_mask ^= 1 << buf_idx;
 
-            if ent.result() == 0 {
-                let sz = self.bufs[buf_idx].stx_size;
-                if self.include_empty || sz != 0 {
-                    self.recorder.record(sz).unwrap();
+            let (dirfd, filename) = self.params[buf_idx].take().unwrap();
+
+            if ent.result() >= 0 {
+                if data >= 0 {
+                    // File `statx`.
+                    let sz = self.bufs[buf_idx].stx_size;
+                    if self.include_empty || sz != 0 {
+                        self.recorder.record(sz).unwrap();
+                    }
+                } else {
+                    // Directory `openat`.
+                    let fd = unsafe { OwnedFd::from_raw_fd(ent.result()) };
+                    let fd = Arc::new(fd);
+                    s.spawn(move |s| WORKER.with(|w| w.borrow_mut().traverse_dir(fd, s)));
                 }
             } else {
                 let err = std::io::Error::from_raw_os_error(-ent.result());
+                let ret = std::fs::read_link(format!("/proc/self/fd/{}", dirfd.as_raw_fd()));
+                let dir_path = ret
+                    .as_ref()
+                    .map_or("<unknown>".into(), |path| path.to_string_lossy());
                 eprintln!(
-                    "failed to get metadata of {}: {}",
-                    self.paths[buf_idx].as_ref().unwrap().to_string_lossy(),
+                    "failed to open {}/{}: {}",
+                    dir_path,
+                    filename.to_string_lossy(),
                     err,
                 );
             }
         }
     }
+
+    fn traverse_dir(&mut self, dirfd: Arc<OwnedFd>, s: &Scope<'_>) {
+        // FIXME: one-file-system
+        let mut dirent_buf = std::mem::take(&mut self.dirent_buf);
+        'done: loop {
+            'resize: {
+                let mut iter = RawDir::new(&dirfd, dirent_buf.spare_capacity_mut());
+                while let Some(entry) = iter.next() {
+                    let entry = match entry {
+                        Ok(entry) => entry,
+                        Err(Errno::INVAL) => break 'resize,
+                        Err(err) => {
+                            eprintln!("fail to traverse {:?}: {}", dirfd, err);
+                            break 'done;
+                        }
+                    };
+                    self.enqueue(dirfd.clone(), entry, s);
+                    while !iter.is_buffer_empty() {
+                        let entry = iter.next().unwrap().unwrap();
+                        self.enqueue(dirfd.clone(), entry, s);
+                    }
+                    self.uring.submit().unwrap();
+                }
+                break 'done;
+            }
+            dirent_buf.reserve(self.dirent_buf.capacity() * 2);
+        }
+
+        self.dirent_buf = dirent_buf;
+        self.finish(s);
+    }
+
+    fn finish(&mut self, s: &Scope<'_>) {
+        let pendings = self.active_mask.count_ones() as usize;
+        self.submit_and_complete(pendings, s);
+    }
 }
 
 fn main() {
-    let mut cli = Cli::parse();
+    let cli = Cli::parse();
     let mut hist = Histogram::<u64>::new(3).unwrap().into_sync();
 
-    if cli.follow_root {
-        cli.root = std::fs::canonicalize(cli.root).unwrap();
-    }
-
-    scoped_tls::scoped_thread_local!(static WORKER: RefCell<Worker>);
-
-    fn spawn_traverse(s: &rayon::Scope, path: PathBuf) {
-        s.spawn(move |s| {
-            // FIXME: one-file-system
-            for ent in std::fs::read_dir(&path).unwrap() {
-                let ent = match ent {
-                    Ok(ent) => ent,
-                    Err(err) => {
-                        eprintln!("fail to traverse {}: {}", path.display(), err);
-                        continue;
-                    }
-                };
-                let file_path = ent.path();
-                if !ent.file_type().unwrap().is_dir() {
-                    WORKER.with(|w| w.borrow_mut().submit(file_path.clone()));
-                } else {
-                    spawn_traverse(s, file_path);
-                }
+    let rootfd = {
+        let mut flags = OFlags::RDONLY | OFlags::DIRECTORY | OFlags::CLOEXEC;
+        flags.set(OFlags::NOFOLLOW, !cli.follow_root);
+        match openat(CWD, &cli.root, flags, Mode::empty()) {
+            Ok(fd) => fd,
+            Err(err) => {
+                eprintln!("failed to open {}: {}", cli.root.display(), err);
+                process::exit(1);
             }
-        });
-    }
+        }
+    };
 
     rayon::ThreadPoolBuilder::new()
         .build_scoped(
@@ -208,7 +270,7 @@ fn main() {
                 WORKER.set(&w, || thread.run());
             },
             |pool| {
-                pool.scope(|s| spawn_traverse(s, cli.root.clone()));
+                pool.scope(|s| WORKER.with(|w| w.borrow_mut().traverse_dir(Arc::new(rootfd), s)));
             },
         )
         .unwrap();
